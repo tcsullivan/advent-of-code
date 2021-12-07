803b9b677fcfc9ca38e1231fcf5def0f35782936
Clyne Sullivan <clyne@bitgloo.com>
2021-12-07 09:24:24 -0500

    day7: learn as->

--- day7/part2.clj
+++ day7/part2.clj
@@ -1,22 +1,19 @@
 (defn calc-fuel [lst pos]
-  (->> lst
-       (map
-         (comp
-           #(/ (* % (inc %)) 2)
-           #(Math/abs %)
-           (partial - pos)
-           )
-         )
-       (apply +)
-       )
+  (reduce #(as-> %2 $
+             (- pos $)
+             (Math/abs $)
+             (/ (* $ (inc $)) 2)
+             (+ %1 $)
+             )
+          0 lst
+          )
   )
 
-(let [input (->> (slurp "./in") ;"16,1,2,0,4,2,7,1,2,14"
-                 (#(clojure.string/split % #","))
-                 (map read-string)
-                 )
-      mean (quot (apply + input) (count input))
-      ]
+(let [input (as-> (slurp "./in") $ ;"16,1,2,0,4,2,7,1,2,14"
+                  (clojure.string/split $ #",")
+                  (map read-string $)
+                  )
+      mean (quot (apply + input) (count input))]
   (println (min (calc-fuel input mean)
                 (calc-fuel input (inc mean))))
   )
