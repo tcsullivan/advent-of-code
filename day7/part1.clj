a1af4e9dec325a3fff03140999ac737c983d1f49
Clyne Sullivan <clyne@bitgloo.com>
2021-12-07 14:45:32 -0500

    day7: improve part1; add maxima solutions

--- day7/part1.clj
+++ day7/part1.clj
@@ -1,22 +1,18 @@
 (defn median [lst]
-  (let [cnt (count lst)
-        hlf (quot cnt 2)
-        srt (sort lst)]
-    (cond->> (nth srt hlf)
-      (even? cnt)
-      (+ (nth srt (dec hlf)))
-      :true
-      (#(quot % 2))
-      )
+  (as-> (count lst) $
+    (quot $ 2)
+    (subvec (vec (sort lst)) (dec $) (inc $))
+    (if (even? (count lst)) (apply + $) (second $))
+    (quot $ 2)
     )
   )
 
-(->> (slurp "./in") ; "16,1,2,0,4,2,7,1,2,14"
-     (#(clojure.string/split % #","))
-     (map read-string)
-     (#(map (partial - (median %)) %))
-     (map #(Math/abs %))
-     (apply +)
-     (println)
-     )
+(as-> (slurp "./in") $ ; "16,1,2,0,4,2,7,1,2,14"
+      (clojure.string/split $ #",")
+      (mapv read-string $)
+      (map (partial - (median $)) $)
+      (map #(Math/abs %) $)
+      (apply + $)
+      (println $)
+      )
 
