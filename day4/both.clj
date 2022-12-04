23effc48309276caead656b366a8bd915196fef5
Clyne Sullivan <clyne@bitgloo.com>
2022-12-04 08:22:52 -0500

    day4: try cleaning up predicates

--- day4/both.clj
+++ day4/both.clj
@@ -1,14 +1,14 @@
+(defn within [n b e] (and (>= n b) (<= n e)))
+
 (defn part1pred [lst]
-  (or (and (>= (nth lst 0) (nth lst 2))
-           (<= (nth lst 1) (nth lst 3)))
-      (and (>= (nth lst 2) (nth lst 0))
-           (<= (nth lst 3) (nth lst 1)))))
+  (or (and (>= (nth lst 0) (nth lst 2)) (<= (nth lst 1) (nth lst 3)))
+      (and (>= (nth lst 2) (nth lst 0)) (<= (nth lst 3) (nth lst 1)))))
 
 (defn part2pred [lst]
-  (or (and (>= (nth lst 0) (nth lst 2)) (<= (nth lst 0) (nth lst 3)))
-      (and (>= (nth lst 1) (nth lst 2)) (<= (nth lst 1) (nth lst 3)))
-      (and (>= (nth lst 2) (nth lst 0)) (<= (nth lst 2) (nth lst 1)))
-      (and (>= (nth lst 3) (nth lst 0)) (<= (nth lst 3) (nth lst 1)))))
+  (or (within (nth lst 0) (nth lst 2) (nth lst 3))
+      (within (nth lst 1) (nth lst 2) (nth lst 3))
+      (within (nth lst 2) (nth lst 0) (nth lst 1))
+      (within (nth lst 3) (nth lst 0) (nth lst 1))))
 
 (def count-filtered (comp println count filter))
 
